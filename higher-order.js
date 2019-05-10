fd4e06a164da715f96709011224627b1c5a32c27
rileyeh <rileyehatch@gmail.com>
2019-05-10 15:27:17 -0600

    thru number 4

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter (function(elem) {
+  return elem % 2 === 0
+})  // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
 
 
@@ -44,7 +46,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(elem){
+  return elem * 1.07
+}) 
+
+// = prices.map(/* Provide Your Callback Here );
 
 
 
@@ -63,7 +69,11 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(acc, elem) {
+  return acc + elem
+})
+
+//  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
@@ -89,7 +99,11 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(elem, i, arr){
+  return elem[1] > 200
+})
+
+// = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
 
 
